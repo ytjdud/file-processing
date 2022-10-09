717b324be3f644c64d5f6db892911fc3ca5622db
윤서영 <102743371+ytjdud@users.noreply.github.com>
2022-10-09 20:25:58 +0900

    Convert pseudocode into java

--- src/bst/BSTbyPseudo.java
+++ src/bst/BSTbyPseudo.java
@@ -24,6 +24,87 @@ public class BSTbyPseudo< K extends Comparable<K>> {
 
     protected INode<K> root;
 
+    public void deleteBST(INode<K> T, K deleteKey){
+        INode<K> p =  T; // root, rt == p in psuedo
+        INode<K> q = null;
+        Stack<INode<K>> stackQ = new Stack<>();
+
+
+        // find position of deleteKey while storing parent node on stack
+        while(p != null && deleteKey != p.key){
+            stackQ.push(q);
+            q = p;
+
+            if (deleteKey.compareTo(p.key) < 0 ){
+                p = p.left();
+            }else{
+                p = p.right();
+            }
+        }
+
+        if (p == null)  return; // deleteKey was not found
+
+        if (p.left() != null && p.right() != null){
+            stackQ.push(p);
+            INode<K> tempNode = p;
+
+            if(p.left().height <= p.right().height){
+                p = p.right();
+                while(p.left() != null){
+                    stackQ.push(p);
+                    p = p.left();
+                }
+            }else{
+                p = p.left();
+                while(p.right() != null){
+                    stackQ.push(p);
+                    p = p.right();
+                }
+            }
+
+            tempNode.key = p.key;
+            q = stackQ.peek();
+        }
+
+        // now degree of p is 0 or 1
+        // delete p from T
+        if(p.left() == null && p.right()==null ){ // case of degree 0
+            if(q ==null){
+                root=null; // case of root ?
+            }else if(q.left() == p){
+                q.l = null;
+            }else{
+                q.r = null;
+            }
+        }else{ // case of degree 1
+            if (p.l != null ){
+                if(q==null){
+                    root = root.left(); // case of root
+                }else if(q.l == p){
+                    q.l = p.l;
+                }else{
+                    q.r = p.l;
+                }
+            }else{
+                if(q == null){
+                    root = root.right(); // case of root
+                }else if(q.l == p){
+                    q.l = p.r;
+                }else{
+                    q.r = p.r;
+                }
+            }
+        }
+
+        // delete p; ? 이거 뭐임?
+
+        // update height while popping parent node from stack
+        while(stackQ != null){
+            q = stackQ.pop();
+            q.height = 1 + Math.max(q.left().height, q.right().height);
+        }
+    }
+
     public void insertBST(INode<K> T, K newKey) {
         INode<K> p =  T; // root, rt == p in psuedo
         INode<K> q = null;
