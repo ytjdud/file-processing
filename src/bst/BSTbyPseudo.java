b6f3a211744ebafbc1afd1e0d97a4ff019d5e283
윤서영 <102743371+ytjdud@users.noreply.github.com>
2022-10-11 18:35:07 +0900

    Merge insertBST

--- src/bst/BSTbyPseudo.java
+++ src/bst/BSTbyPseudo.java
@@ -6,28 +6,52 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class BSTbyPseudo< K extends Comparable<K>> {
-    public void main(String[] args) throws IOException {
+final class INode<Integer> {
+    int height;
+    int key;
+    INode<Integer> l, r;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String order = st.nextToken();
-        String key = st.nextToken();
+    public INode(INode<Integer> l, int key, int height, INode<Integer> r){
+        this.key = key;
+        this.l = l;
+        this.r = r;
+        this.height = height;
+    }
 
-        if(order == "i"){
-            insertBST(null, (K) key);
-        }else{
-            // deleteBST()
+    public INode<Integer> left() {
+        return l;
+    }
+    public INode<Integer> right() {
+        return r;
+    }
+
+}
+
+public class BSTbyPseudo {
+    public static void main(String[] args) throws IOException {
+
+        root = null;
+        while(true) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+            int key = Integer.parseInt(st.nextToken());
+
+            if (order.equals("i")) {
+                root = insertBST(root, key);
+            } else {
+                // deleteBST()
+            }
         }
 
     }
 
-    protected INode<K> root;
+    protected static INode<Integer> root;
 
-    public void deleteBST(INode<K> T, K deleteKey){
-        INode<K> p =  T; // root, rt == p in psuedo
-        INode<K> q = null;
-        Stack<INode<K>> stackQ = new Stack<>();
+    public void deleteBST(INode<Integer> T, int deleteKey){
+        INode<Integer> p =  T; // root, rt == p in psuedo
+        INode<Integer> q = null;
+        Stack<INode<Integer>> stackQ = new Stack<>();
 
 
         // find position of deleteKey while storing parent node on stack
@@ -35,7 +59,7 @@ public class BSTbyPseudo< K extends Comparable<K>> {
             stackQ.push(q);
             q = p;
 
-            if (deleteKey.compareTo(p.key) < 0 ){
+            if (deleteKey<p.key){
                 p = p.left();
             }else{
                 p = p.right();
@@ -46,7 +70,7 @@ public class BSTbyPseudo< K extends Comparable<K>> {
 
         if (p.left() != null && p.right() != null){
             stackQ.push(p);
-            INode<K> tempNode = p;
+            INode<Integer> tempNode = p;
 
             if(p.left().height <= p.right().height){
                 p = p.right();
@@ -105,58 +129,70 @@ public class BSTbyPseudo< K extends Comparable<K>> {
         }
     }
 
-    public void insertBST(INode<K> T, K newKey) {
-        INode<K> p =  T; // root, rt == p in psuedo
-        INode<K> q = null;
-        Stack<INode<K>> stackQ = new Stack<>();
+    public static INode<Integer> insertBST(INode<Integer> T, int newKey) {
+        INode<Integer> p =  T; // root, rt == p in psuedo
+        INode<Integer> q = null;
+        Stack<INode<Integer>> stackQ = new Stack<>();
 
         while(p != null){ //  step 1: newKey의 삽입위치q를 찾음.
-            if(newKey == p.key)   return;
+            if(newKey == p.key) {
+                System.out.printf("i %d : The key already exists\n", newKey);
+                return T;
+            }
 
+            q = p; // 이게 먼저인 이유 : 이후 while(stack is not null) height++; 해줄거라
             stackQ.push(q);
-            q = p;
 
-            if(newKey.compareTo(p.key) < 0){
+            if(newKey < p.key ){
                 p = p.left();
             } else{
                 p = p.right();
             }
         }
 
-        INode<K> newNode = new INode<>(null, newKey, 0, null);
+        INode<Integer> newNode = getBSTNode();
+        newNode.key = newKey;
 
-        if(p == null){ // step 3: q의 자식으로 newkey를 삽입.
-            p = newNode;
-        } else if (newKey.compareTo(q.key) < 0) {
+        if(T == null){ // step 3: q의 자식으로 newkey를 삽입.
+            T = newNode;
+        } else if (newKey< q.key) {
             q.l = newNode;
         } else{
             q.r = newNode;
         }
 
-        while(stackQ != null){
-            q.key = stackQ.pop().key;
-            q.height = 1 + Math.max(q.left().height, q.right().height);
-        }
+        // update height while popping parent node from stack
+        int size = stackQ.size();
 
+        while(size-- != 0){
+            q = stackQ.pop();
+            if(q.l == null){
+                q.height = 1 + q.r.height;
+            } else if (q.r == null) {
+                q.height = 1 + q.l.height;
+            }else {
+                q.height = 1 + Math.max(q.l.height, q.r.height);
+            }
+        }
+        // TODO : (1) q.l.height 가 nullpointexception 일때 예외처리 (2) optional
+
+        inorder(T);
+        System.out.println();
+
+        return T;
     }
 
-    public static class INode<K> {
-        private int height;
-        private K key;
-        private INode<K> l, r;
+    private static INode<Integer> getBSTNode() {
+        return new INode<Integer>(null, 0, 0,null);
+    }
 
-        public INode(INode<K> l, K key, int height, INode<K> r){
-            this.key = key;
-            this.l = l;
-            this.r = r;
-            this.height = height;
+    static public <K> void inorder(INode<K> rt) {
+        if(rt == null) {
+            // TODO: stdout 말고 스트링 빌더나 버퍼아웃 쓰면 백스페이스 구현가능
+            return;
         }
-
-        public INode<K> left() {
-            return l;
-        }
-        public INode<K> right() {
-            return r;
-        }
+        inorder(rt.left());
+        System.out.print(rt.key+ " " ); //visit
+        inorder(rt.right());
     }
 }
