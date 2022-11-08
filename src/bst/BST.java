43f995def8d6bf52c3fa855259c4bda4086c99ea
윤서영 <102743371+ytjdud@users.noreply.github.com>
2022-11-08 11:18:34 +0900

    Change input format and Fix stack update issue

--- src/bst/BST.java
+++ src/bst/BST.java
@@ -1,8 +1,6 @@
 package bst;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -13,10 +11,10 @@ final class INode<Integer> {
     INode<Integer> l, r;
 
     public INode(INode<Integer> l, int key, int height, INode<Integer> r){
-        this.key = key;
         this.l = l;
-        this.r = r;
+        this.key = key;
         this.height = height;
+        this.r = r;
     }
 
     public INode<Integer> left() {
@@ -28,23 +26,32 @@ final class INode<Integer> {
 
 }
 
-public class BSTbyPseudo {
+public class BST {
     public static void main(String[] args) throws IOException {
 
         root = null;
-        while(true) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BufferedReader br = new BufferedReader(
+                new FileReader("src/input.txt")
+        );
+        String str;
+
+        while((str = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(str);
+
             String order = st.nextToken();
             int key = Integer.parseInt(st.nextToken());
 
             if (order.equals("i")) {
                 root = insertBST(root, key);
+                inorder(root);
+                System.out.println();
             } else {
                 root = deleteBST(root, key);
+                inorder(root);
+                System.out.println();
             }
         }
-
     }
 
     protected static INode<Integer> root;
@@ -53,9 +60,7 @@ public class BSTbyPseudo {
         INode<Integer> p =  T; // root, rt == p in psuedo
         INode<Integer> q = null;
         Stack<INode<Integer>> stackQ = new Stack<>();
-
-
-        // find position of deleteKey while storing parent node on stack
+        // find
         while(p != null && deleteKey != p.key){
 
             q = p;
@@ -66,47 +71,24 @@ public class BSTbyPseudo {
             }else{
                 p = p.right();
             }
-        }
+        } // delete 할 대상까지 p q 내려감
 
         if (p == null)  {
             System.out.printf("d %d : The key does not exist\n", deleteKey);
-            return T; // deleteKey was not found
+            return T; // (1) deleteKey was not found
         }
 
-        if (p.l != null && p.r != null){
-            stackQ.push(p);
-            INode<Integer> tempNode = p;
-
-            if(p.l.height <= p.r.height){
-                p = p.right();
-                while(p.l != null){
-                    stackQ.push(p);
-                    p = p.left();
-                }
-            }else{
-                p = p.left();
-                while(p.r != null){
-                    stackQ.push(p);
-                    p = p.right();
-                }
-            }
-
-            tempNode.key = p.key; // tempNode 가 하는게 뭐야
-            q = stackQ.peek();
-        }
-
-        // now degree of p is 0 or 1
         // delete p from T
         if( p.l == null && p.r == null ){ // case of degree 0
-            if(q == null){
+            if(q == null){ // (2) 루트 노드만 있는 경우, 루트를 삭제
                 T = null;
-            }else if(q.l == p){
+            }else if(q.l == p){ // (3)
                 q.l = null;
-            }else{
+            }else{ // (3)
                 q.r = null;
             }
-        }else{ // case of degree 1
-            if (p.l != null ){
+        }else if (p.l == null || p.r == null) { // (4) case of degree 1
+            if (p.l != null ){ // p.l 에 자식이 있을때
                 if(q==null){
                     T = T.l; // case of root
                 }else if(q.l == p){
@@ -123,29 +105,94 @@ public class BSTbyPseudo {
                     q.r = p.r;
                 }
             }
+        } else if (p.l != null && p.r != null){ // 삭제할 노드의 차수가 2인 경우. 왼오 모두 자식을 가지고 있을 경우
+
+            INode<Integer> r = null;
+            // height() : 본인을 제외하고 서브트리의 높이
+            // noNode() : 본인(파라미터)을 포함하는 노드의 개수
+
+            if ( p.l.height > p.r.height ) {
+                r = maxNode(p, q, stackQ);
+                p.key = r.key;
+                p.l = deleteBST(p.left(), r.key);
+            }else if ( p.l.height < p.r.height ){
+                r = minNode(p, q, stackQ);
+                p.key = r.key;
+                p.r = deleteBST(p.right(), r.key);
+            }else{ // p.l.height == p.r.height
+                if( noNodes(p.left()) >= noNodes(p.right()) ){
+                    r = maxNode(p, q, stackQ);
+                    p.key = r.key;
+                    p.l = deleteBST(p.left(), r.key);
+                }else{
+                    r = minNode(p, q, stackQ);
+                    p.key = r.key;
+                    p.r = deleteBST(p.right(), r.key);
+                }
+            }
         }
+        // 여기서 계속 내려가야함 -> p q stack
 
-        // delete p; <- p will delete automatically by java garbage collector
-
-        // update height while popping parent node from stack
         int size = stackQ.size();
 
         while(size-- != 0){
             q = stackQ.pop();
-            if(q.l == null && q.r == null){
-                q.height = 0 ;
-            }else if (q.l == null) {
-                q.height = q.r.height + 1 ;
+            if(q.l == null && q.r == null ){
+                q.height = 0;
+            } else if(q.l == null){
+                q.height = 1 + q.r.height;
+            } else if (q.r == null) {
+                q.height = 1 + q.l.height;
             }else {
-                q.height = q.l.height + 1;
+                q.height = 1 + Math.max(q.l.height, q.r.height);
             }
         }
+
         // TODO : (1) q.l.height 가 nullpointexception 일때 예외처리 (2) optional
 
-        inorder(T);
-        System.out.println();
+
 
         return T;
+    }
+
+    private static int noNodes(INode<Integer> rt) {
+        // 재귀로 풀면 되는거 아님?
+        // 아 inorder 에 프린트 대신 cnt 하면 되는 거 아님?
+        int cnt =0;
+
+        if(rt == null) {
+            return 0;
+        }
+        inorder(rt.left());
+        cnt++; //visit
+        inorder(rt.right());
+
+        return 0;
+    }
+
+    private static INode<Integer> maxNode(INode<Integer> p, INode<Integer> q, Stack<INode<Integer>> stackQ) {
+        q = p;
+        stackQ.push(q);
+
+        p = p.left();
+        while(p.right() != null){
+            stackQ.push(p);
+            p = p.right();
+        }
+        return p;
+    }
+
+    private static INode<Integer> minNode(INode<Integer> p, INode<Integer> q, Stack<INode<Integer>> stackQ) {
+        q = p;
+        stackQ.push(q);
+
+        p = p.right();
+        while(p.left() != null){
+            q = p;
+            stackQ.push(p);
+            p = p.left();
+        }
+        return p;
     }
 
 
@@ -194,10 +241,7 @@ public class BSTbyPseudo {
                 q.height = 1 + Math.max(q.l.height, q.r.height);
             }
         }
-        // TODO : (1) q.l.height 가 nullpointexception 일때 예외처리 (2) optional
-
-        inorder(T);
-        System.out.println();
+        // TODO : (1) q.l.height 가 nullpointexception 일때 예외처리 (2) optional (null 검사 안하기 <- 효율성 떨어짐)
 
         return T;
     }
